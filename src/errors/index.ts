fdf79d8dfca462489ff8f17eefae7e6c07dc016c
Mathias Andresen <dev@andresen.xyz>
2019-11-04 23:10:28 +0100

    error documentation

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,8 +1,42 @@
+/**
+ * Thrown when a source file is malformed
+ * @category JabError
+ * @extends {Error}
+ */
 export class MalformedSourceFileError extends Error { }
+
+/**
+ * Thrown when an IO error has happened
+ * @category JabError
+ * @extends {Error}
+ */
 export class IOError extends Error { }
+
+/**
+ * Thrown when an error has happened in a JabTable
+ * @category JabError
+ * @extends {Error}
+ */
 export class JabTableError extends Error { }
+
+/**
+ * Thrown when an error has happened in a JabDB
+ * @category JabError
+ * @extends {Error}
+ */
 export class JabDBError extends Error { }
+
+/**
+ * Thrown when the table searched for was not found
+ * @category JabError
+ * @extends {JabDBError}
+ */
 export class JabTableNotFoundError extends JabDBError {
+    /**
+     * The id of the table that was not found
+     * @type {string}
+     * @memberof JabTableNotFoundError
+     */
     id: string;
 
     constructor(id: string) {
